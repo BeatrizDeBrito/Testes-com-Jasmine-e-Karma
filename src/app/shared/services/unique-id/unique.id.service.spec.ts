0f7623c3bc5777ae5865935022f5360bcb4f079d
Beatriz de Brito <beatrizdb@ciandt.com>
2023-01-26 10:32:14 -0300

    Altera para apenas uma chamada do service

--- src/app/shared/services/unique-id/unique.id.service.spec.ts
+++ src/app/shared/services/unique-id/unique.id.service.spec.ts
@@ -1,10 +1,21 @@
 import { UniqueIdService } from './unique-id.service';
+const service = new UniqueIdService();
 describe(UniqueIdService.name, () =>{
   it('#${UniqueIdService.protype.generateUniqueIdWithPrefix.name} should generate id when called with prefix',() => {
-    const service = new UniqueIdService();
     const id = service.generateUniqueIdWithPrefix('app');
-    expect(id).toContain('app-')
-
+    expect(id.startsWith('app-')).toBeTrue();
   });
+  it('#${UniqueIdService.protype.generateUniqueIdWithPrefix.name} should not generate id when called with prefix',() =>{
+    const ids = new Set();
+    for(let i = 0; i < 50; i++){
+      ids.add(service.generateUniqueIdWithPrefix('app'));
+    }
+    expect(ids.size).toBe(50);
+  })
 
+  it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name} should return the number of generatedIds when called`,() =>{
+    service.generateUniqueIdWithPrefix('app');
+    service.generateUniqueIdWithPrefix('app');
+    expect(service.getNumberOfGeneratedUniqueIds()).toBe(2)
+  })
   });
